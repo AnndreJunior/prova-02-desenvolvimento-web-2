2281dff93e91f7faf6b86ce3c3a09eacccdb8c70
AnndreJunior <anndre.junior15@gmail.com>
2025-06-09 20:28:29 -0300

    configura a conexão do servidor com o banco mysql

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -2,6 +2,10 @@ const { z } = require('zod')
 
 const envSchema = z.object({
   PORT: z.string(),
+  DB_HOST: z.string(),
+  DB_USER: z.string(),
+  DB_PASS: z.string(),
+  DB_NAME: z.string(),
 })
 
 const env = envSchema.parse(process.env)
