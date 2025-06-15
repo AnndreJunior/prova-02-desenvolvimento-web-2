07857d1336763c15b2fcd98d60147af0e2c95dee
GabyAzevedo <gabrielly.azevedo09@aluno.ifce.edu.br>
2025-06-15 20:45:38 -0300

    Listar rotas

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
-
+const {listarTarefas} = require('../controllers/tarefaController')
 router.get('/hello-world', (_, res) => {
   res.json({ message: 'Hello World!' })
 })
-
+router.get('/tarefas', listarTarefas)
 module.exports = router
